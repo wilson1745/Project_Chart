22ae912fa9d8238a49a03090dd31aaa9063d43c4
wilson1745 <wilson-power0130@hotmail.com>
2018-06-15 11:45:42 +0100

    Manage to set XAxis

---
+++ app/src/main/java/e/wilso/project_chart/LineChartActivity.java
@@ -0,0 +1,13 @@
+package e.wilso.project_chart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LineChartActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_line_chart);
+   }
+}
